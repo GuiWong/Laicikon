0bf95d0a66c14639732eeef6ff40aa418ab10e36
GuiWong <33897640+GuiWong@users.noreply.github.com>
2019-09-14 17:37:22 +0200

    basic UI work commit

--- Main.py
+++ Main.py
@@ -14,6 +14,7 @@ class Lexicon_App:
 		self.window=tk.Tk()
 		self.window.title('Lexicon manager version  ' + VERSION)
 
+		self.window.geometry('700x500')
 		self.win_width=self.window.winfo_width()
 
 		self.menu=tk.Menu(self.window)
@@ -30,17 +31,19 @@ class Lexicon_App:
 
 		self.left_frame=tk.Frame(self.window,width=(150),bg='cyan')
 		self.tools_frame=tk.Frame(self.left_frame,height=40,bg='brown')
-		self.list_frame=tk.Frame(self.left_frame,height=420)
-		self.main_frame=tk.Frame(self.window,width=(500),height=460)
+		self.list_frame=tk.Frame(self.left_frame)
+		self.main_frame=tk.Frame(self.window,bg='green')
 
 		self.left_frame.pack(side=tk.LEFT,fill='y')
 		self.left_frame.pack_propagate(0)
 		self.tools_frame.pack(fill='x')
 		self.tools_frame.pack_propagate(0)
-		self.list_frame.pack(fill='x')
+		self.list_frame.place(x=0,y=40,relwidth=1,relheight=1)#fill='both')
 		self.list_frame.pack_propagate(0)
-		self.main_frame.pack(side=tk.LEFT)
-		self.main_frame.pack_propagate(0)
+		
+		self.main_frame.place(x=150,y=0,relwidth=1,relheight=1)
+		#self.main_frame.pack(side=tk.LEFT,fill='both')
+		#self.main_frame.pack_propagate(0)
 
 		self.tabs=ttk.Notebook(self.main_frame)
 		self.derivation_tab=ttk.Frame(self.tabs)
@@ -52,6 +55,8 @@ class Lexicon_App:
 		self.tabs.pack(expand=1, fill='both')
 
 		#pack_propagate(0)
+
+		print self.window.winfo_height()
 
 		self.window.mainloop()
 
